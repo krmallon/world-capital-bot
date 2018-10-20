791c8f4005a858d6d03166b8d34b8316ab14bf77
Kevin <kevinmallon.uk@gmail.com>
2018-10-20 23:22:21 +0100

    Create main bot functionality

---
+++ worldcapitalbot.py
@@ -0,0 +1,39 @@
+# worldcapitalbot
+
+import praw
+import config
+
+# reddit API login details
+reddit = praw.Reddit(client_id = config.client_id,
+                     client_secret = config.client_secret,
+                     username = config.username,
+                     password = config.password,
+                     user_agent = config.user_agent)
+
+# subreddit that the bot operates on
+subreddit = reddit.subreddit('test')
+
+# phrase that activates the bot
+keyphrase = '!worldcapital '
+
+# dictionary of countries and their capital cities
+world_capitals = {"Afganistan": "Kabul", "Albania": "Tirana",
+                  "Algeria": "Algiers", "Andorra": "Andorra la Vella",
+                  "Angola": "Luanda", "Antigua and Barbuda": "Saint John’s",
+                  "Argentina": "Buenos Aires", "Armenia": "Yerevan",
+                  "Australia": "Canberra", "Austria": "Vienna",
+                  "Azerbaijan": "Baku", "Bahamas": "Nassau",
+                  "Bahrain": "Manama", "Bangladesh": "Dhaka",
+                  "Barbados": "Bridgetown", "Belarus": "Minsk",
+                  }
+
+# read country name from comment and search dictionary
+for comment in subreddit.stream.comments():
+    if keyphrase in comment.body:
+        word = comment.body.replace(keyphrase, '')
+        reply = 'Capital city: ' + world_capitals[word]
+        comment.reply(reply)
+    else:
+        reply = 'No result found.'
+        comment.reply(reply)
+            
